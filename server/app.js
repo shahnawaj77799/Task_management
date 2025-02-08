a192fcf7dc89654ec8c70391f4f9b95fdcffb52f
shahnawaj ali <shahnawajali323@gmail.com>
2025-02-08 17:10:52 +0530

    UP

--- server/app.js
+++ server/app.js
@@ -2,12 +2,20 @@ const express= require("express");
 const app= express();
 const cors= require("cors");
 const mongoose=require("mongoose");
+require("dotenv").config();
 const adminRoute= require("./routes/adminRoute");
 const empRoute= require("./routes/employeeRoute");
 const bodyparser = require('body-parser');
-mongoose.connect("mongodb://127.0.0.1:27017/pm6task").then(()=>{
-    console.log("DB connected!");
-})
+const port =process.env.PORT ||8080;
+const MNDB=process.env.DBCONN;
+mongoose
+  .connect(MNDB)
+  .then(() => {
+    console.log("DB Connected!!!");
+  })
+  .catch((err) => {
+    console.error("DB Connection Error:", err);
+  });
 app.use(cors());
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json())
@@ -18,6 +26,6 @@ app.use("/employee", empRoute);
 
 
 
-app.listen(8080, ()=>{
+app.listen(port, ()=>{
     console.log("Server run on 8080 port!");
 })
